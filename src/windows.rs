02cd296fb7136ba9ddf183c2c8332cddfef1012f
xuxiaocheng <2762267080@qq.com>
2023-12-17 00:22:57 +0800

    fix(Cargo): 移动windows-upgrader的位置

--- src/windows.rs
+++ src/windows.rs
@@ -53,9 +53,11 @@ pub fn create_process(command: &Command, runtime_directory: &str) -> Result<()> 
     Ok(())
 }
 
-pub(crate) fn call_upgrader(source: &str, target: &str, runtime: &str, delete: bool, args: &Vec<&str>) -> Result<()> {
+pub fn call_upgrader(source: &str, target: &str, runtime: &str, delete: bool, args: &Vec<&str>) -> Result<()> {
     let mut upgrader = OpenOptions::new().write(true).create(true).truncate(true).open("upgrader.exe")?;
-    upgrader.write_all(include_bytes!("../windows-upgrader/target/release/windows-upgrader.exe"))?;
+    // Please see: https://github.com/xuxiaocheng0201/upgrade/tree/master/windows-upgrader
+    // After you build 'windows-upgrader' (cargo build --release), please replace it (copy /y .\windows-upgrader\target\release\windows-upgrader.exe .\windows-upgrader.exe).
+    upgrader.write_all(include_bytes!("../windows-upgrader.exe"))?;
     upgrader.flush()?;
     drop(upgrader);
     create_process(&Command::new("./upgrader.exe").arg(source).arg(target).arg(runtime)
