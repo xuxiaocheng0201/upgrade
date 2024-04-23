b4b540dc0c1e10a8d65ae2d88321b768aa073e61
xuxiaocheng <2762267080@qq.com>
2024-04-23 11:52:19 +0800

    feat(upgrade): 优化入口参数

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,31 @@
-use std::env::current_exe;
-use std::fs::remove_file;
-use std::path::Path;
-use std::process::Command;
-use anyhow::{anyhow, Result};
-use self_replace::self_replace;
-
-pub mod builder;
-
-pub extern crate anyhow;
 pub extern crate self_replace;
 
-/// Please consider using `upgrade::builder::Builder`
-#[doc(hidden)]
-pub fn run_upgrade<P: AsRef<Path>>(source: P, delete: bool, args: &Vec<&str>) -> Result<()> {
+use std::env::current_exe;
+use std::ffi::OsStr;
+use std::fs::remove_file;
+use std::io::Result;
+use std::path::Path;
+use std::process::Command;
+
+use self_replace::self_replace;
+
+/// Replace the current exe with the source path.
+///
+/// You may exit the program as soon as possible after it returns Ok.
+///
+/// See: [`Command`] and [`Command::args`]
+pub fn run_upgrade<P: AsRef<Path>, S: AsRef<OsStr>, A: IntoIterator<Item=S>>(source: P, delete: bool, args: A) -> Result<()> {
     let source = source.as_ref();
     self_replace(source)?;
     let current_exe = current_exe()?;
-    let current_exe = current_exe.to_str()
-        .ok_or(anyhow!("Invalid current exe: {:?}", current_exe))?;
-    Command::new(current_exe).args(args).spawn()?;
+    Command::new(&current_exe).args(args).spawn()?;
     if delete { remove_file(source)?; }
     Ok(())
 }
 
-/// Replace the current exe with the param path.
+/// A shortcut of [`run_upgrade`] without args.
 ///
 /// You may exit the program as soon as possible after it returns Ok.
 pub fn upgrade<P: AsRef<Path>>(path: P) -> Result<()> {
-    run_upgrade(path, true, &Vec::new())
+    run_upgrade::<_, &str, _>(path, true, [])
 }
