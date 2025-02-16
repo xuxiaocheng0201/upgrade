5f1807be3750f38f4db9867465f0d16595f42c2f
xuxiaocheng <2762267080@qq.com>
2025-02-16 15:03:30 +0800

    fix(lib): 修复android(termux)无法找到文件的错误

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,6 @@ use std::io::Result;
 use std::path::Path;
 use std::process::Command;
 
-use self_replace::self_replace;
-
 /// Replace the current exe with the source path.
 ///
 /// You may exit the program as soon as possible after it returns Ok.
@@ -20,9 +18,9 @@ use self_replace::self_replace;
 /// See: [`Command`] and [`Command::args`]
 pub fn run_upgrade<P: AsRef<Path>, S: AsRef<OsStr>, A: IntoIterator<Item=S>>(source: P, delete: bool, args: A) -> Result<()> {
     let source = source.as_ref();
-    self_replace(source)?;
-    let current_exe = current_exe()?;
-    Command::new(&current_exe).args(args).spawn()?;
+    let current = current_exe()?.canonicalize()?;
+    self_replace::self_replace(source)?;
+    Command::new(&current).args(args).spawn()?;
     if delete { remove_file(source)?; }
     Ok(())
 }
@@ -30,6 +28,7 @@ pub fn run_upgrade<P: AsRef<Path>, S: AsRef<OsStr>, A: IntoIterator<Item=S>>(sou
 /// A shortcut of [`run_upgrade`] without args.
 ///
 /// You may exit the program as soon as possible after it returns Ok.
+#[inline]
 pub fn upgrade<P: AsRef<Path>>(path: P) -> Result<()> {
     run_upgrade::<_, &str, _>(path, true, [])
 }
